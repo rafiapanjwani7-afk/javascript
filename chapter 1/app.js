e0708b6ad60ced7e99bbea5381e6c2d73fb335ba
rafiapanjwani7-afk <rafiapanjwani7@gmail.com>
2025-12-22 13:38:23 +0500

    javascript

---
+++ chapter 1/app.js
@@ -0,0 +1,17 @@
+// 1. Greet website visitor
+alert("Welcome to my website!");
+
+// 2. error message
+alert("Error! Please enter a valid username.");
+
+// 3. Line break message
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// 4. Messages in sequence
+alert("Welcome to JS Land...");
+alert("Happy Coding!\n Prevent this page from creating additional dialogs.");
+
+// 5. Message in browser console
+console.log("Hello... I can run JS through my web browser's console");
+// page--content
+document.write("JavaScript Alerts Assignment \n This page is for Assignment # 1 ");
---
+++ chapter 2/app.js
@@ -0,0 +1,25 @@
+//   username
+  var MyName = "Rafia samiullah"
+console.log(MyName); 
+alert(MyName)
+ var massage = "hello world"
+console.log(massage); 
+alert(massage)
+var app = "Certified Mobile Aplication Development"
+console.log(app); 
+alert(app)
+var pizza= " PIZZA \n PIZZ \n PLZ \n PL \n P"
+console.log(pizza); 
+alert(pizza)
+var email = "rafiapanjwani7@gmail.com"
+console.log(email); 
+alert(email)
+var As= "I am trying to learn from the Book A smarter way to learn Javascript"
+console.log(As); 
+alert(As)
+// page--content
+document.write("Yah! I can write HTML content through JavaScript ");
+
+var design= "*=====ஜ⚖️101⚖️ஜ=======*";
+alert(design);
+document.write(design);
