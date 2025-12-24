d856bbfb89818b2f07132423829bc059c370666d
rafiapanjwani7-afk <rafiapanjwani7@gmail.com>
2025-12-24 13:16:59 +0500

    chap 3

---
+++ chapter 3/app.js
@@ -0,0 +1,32 @@
+//  js-assignment variable & number:
+// ===== Question 1 ===== 
+
+var myage = " 15"
+alert(" I am" +myage+  " year old ")
+//  ===== Question 2 ===== 
+
+var times = " 14 times"
+ var vist= "visted"
+console.log("you have " +vist + " this site" +times)
+alert("you have " +vist + " this site" +times)
+
+// ===== Question 3 ===== 
+
+var  MybirthYear = 1990;  
+ document.write("<p>My birth year is " + MybirthYear + "</p>");
+        document.write("<p>Data type of my declared variable is number</p>");
+
+// ===== Question 4 ===== 
+
+ var visitorName = "John Doe";
+        var ProductTitle = "T-shirt(s)";
+        var Quantity = 5;
+
+        document.write(
+            visitorName + " ordered " + Quantity + +
+            ProductTitle + " on XYZ Clothing store"
+        );
+
+       
+
+       
