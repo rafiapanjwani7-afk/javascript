9bbe3cdb45064d460b6b4e7175a41272ecd534c5
rafiapanjwani7-afk <rafiapanjwani7@gmail.com>
2025-12-24 13:51:53 +0500

    javascript

---
+++ chapter 5/app.js
@@ -0,0 +1,25 @@
+// js-assignment-math-expression:
+// question1-addition
+var num1 = 6;
+var num2 = 9;
+var num = num1 + num2;
+document.write("Sum of " + num1 + " and " + num2 + " is " + num + "<br>");
+// question2- Subtraction
+var num1 = 12;
+var num2 = 3;
+var subtractionResult = num1 - num2;
+document.write("Subtraction of " + num1 + " and " + num2 + " is " + subtractionResult + "<br>");
+
+// question 3- Multiplication
+var multiplicationResult = num1 * num2;
+document.write("Multiplication of " + num1 + " and " + num2 + " is " + multiplicationResult + "<br>");
+
+// question 4- Division
+var divisionResult = num1 / num2;
+document.write("Division of " + num1 + " and " + num2 + " is " + divisionResult + "<br>");
+
+// question 5- Modulus (remainder)
+var num1 =15
+var num2 = 7
+var modulusResult = num1 % num2;
+document.write("Modulus of " + num1 + " and " + num2 + " is " + modulusResult + "<br>");
