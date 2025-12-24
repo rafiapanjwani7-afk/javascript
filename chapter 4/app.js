79255c9b1fea0a37460509e9fb52343fc9bc9573
rafiapanjwani7-afk <rafiapanjwani7@gmail.com>
2025-12-24 13:40:45 +0500

    javascript

---
+++ chapter 4/app.js
@@ -0,0 +1,30 @@
+// js-assignment-var-legal-&-illegal:
+//  question- 1 declared 3 varaible statement ?
+    var StudentName = "Aliya", age = 16,country = "pakistan"
+
+    //  question 2: 5 Legal variable names?
+    var  Myname = "amna";
+    var Myage= 20;
+    var $price = 500;
+    var userId = "valid";
+    var camelCaseVariable = "good practice";
+
+    //  question 2: 5 Illegal variable names (commented to avoid ?errors)
+    // var 123user;          // starts with number
+    // var my-name;          // contains hyphen
+    // var let;              // reserved keyword
+    // var first name;       // contains space
+    // var @email;           // invalid special character
+
+    //question 3: Display rules in the browser?
+    document.write("<h1>  js-assignment</h1>");
+
+    document.write("<p>Variable names can only contain <strong>numbers , $ letters, and _</strong>.<br>");
+    document.write(" For example: <strong>$my_1stVariable</strong></p>");
+
+    document.write("<p>Variables must begin with a <strong>letter, $ or _</strong>.<br>");
+    document.write("For example: <strong>$name</strong>, <strong>_name</strong> or <strong>name</strong></p>");
+
+    document.write("<p>Variable names are <strong>case variable</strong></p>");
+
+    document.write("<p>Variable names should not be JS <strong>keywords</strong></p>");
