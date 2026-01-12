63ca2c29ac4a4565587f7e9288998a49f2e3f45a
rafiapanjwani7-afk <rafiapanjwani7@gmail.com>
2026-01-12 11:51:51 +0500

    array and loops

---
+++ chapter 17-20/app.js
@@ -0,0 +1,115 @@
+//--------------- ARRAY AND LOOPS ----------------//
+// Question 1:
+var Multi$Arr = [[], [], []];
+ 
+// Question 2;
+document.write("<h1> matrix: </h1>")
+Multi$Arr = [[0, 1, 2, 3][1, 0, 1, 2][2, 1, 0, 1]]
+for (var i = 0; i < 4; i++) {
+    document.write(i + "<br>")
+}
+
+// Question 3
+document.write(`<h1>COUNTING:</h1>`)
+var Count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (i = 1; i < 11; i++) {
+    document.write(i + "<br>")
+}
+
+
+//  Question 4
+document.write(`<h1>tables</h1>`)
+var Tables = +prompt("Enter Number To Show Its Multiplication Table:") || 5;
+var Length = +prompt("Enter Length Multiplication Table:") || 10;
+for (var i = 1; i <= Length; i++) {
+    document.write(Tables + "x" + i + "=" + Tables * i + "</br>")
+    console.log(Tables)
+}
+
+
+// Question 5
+document.write(`<h1> Fruits Element:</h1>`)
+var Fruit = ["apple", "banana", "mango", "orange", "strawberry"]
+for (var i = 0; i < Fruit.length; i++) {
+    document.write("<p> elment at index " + i + " is" + " " + Fruit[i] + "</p>")
+}
+
+
+// Question 6:
+document.write(`<h1>Counting:</h1>`)
+for (var i = 0; i <= 15; i++) {
+    document.write(i + " ,")
+
+}
+document.write(`</br>`)
+
+document.write(`<h1>Reverse Counting:</h1>`)
+for (var i = 10; i > 1; i--) {
+    document.write(i + " ,")
+}
+document.write(`</br>`)
+document.write(`<h1>Even counting:</h1>`)
+for (var i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        document.write(i + " ,")
+    }
+}
+document.write(`</br>`)
+document.write(`<h1>Odd counting:</h1>`)
+for (var i = 0; i < 20; i++) {
+    if (i % 2 !== 0) {
+        document.write(i + " ,")
+    }
+}
+document.write(`</br>`)
+document.write(`<h1>Series:</h1>`)
+for (var i = 2; i <= 20; i++) {
+    if (i % 2 === 0) {
+        document.write(i + "k ,")
+    }
+}
+document.write(`</br>`)
+
+
+// Question 7
+document.write(`<h1>bakery:</h1>`)
+var bak_item = ["cake", "apple pie", "cookies", "chips", "patties"]
+var userOrder = prompt("Weclome To ABC bakery. What Do You Want To order Sir/Ma'am?")
+for (i = 0; i < bak_item.length; i++) {
+    if (userOrder.toLowerCase() === bak_item[i]) {
+        document.write("<p>" + bak_item[i] + " is <strong> available </strong> at index " + bak_item.length + " in our bakery </p>")
+    }
+
+}
+
+//  Question 8
+document.write(`<h1> biggest Number:</h1>`)
+var arr = [24, 53, 78, 91, 12]
+document.write(`<strong>Array Items:</strong> ${arr} <br>`)
+var Largest = [0]
+for (i = 0; i < arr.length; i++) {
+    if (arr[i] > Largest) {
+        Largest = arr[i];
+    }
+}
+document.write("<p>The largest number is " + Largest + "</p>");
+
+
+//  Question 9
+document.write(`<h1> smaller Number:</h1>`)
+var arr = [24, 53, 78, 91, 12]
+document.write(`<strong>Array Items:</strong> ${arr} <br>`)
+var Smallest = [0]
+for (i = 0; i > arr.length; i++) {
+    if (arr[i] < Smallest) {
+        Smallest = arr[i];
+    }
+}
+document.write("<p>The Smallest number is " + Smallest + "</p>");
+
+
+//  Question 10
+document.write("<h1>Ranging Counting:</h1>")
+for (var i = 5; i <= 100; i += 5) {
+    document.write(i + " ,")
+}
