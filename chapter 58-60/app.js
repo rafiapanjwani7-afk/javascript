3ff96324915fae384c2456dbee27f941a251a64b
rafiapanjwani7-afk <rafiapanjwani7@gmail.com>
2026-03-06 13:31:17 +0500

    Dom'

--- chapter 58-60/app.js
+++ chapter 58-60/app.js
@@ -11,3 +11,24 @@ document.getElementById("first-name").value="Alex";
 document.getElementById("last-name").value="Bank";
 document.getElementById("email").value="alexbank@wexample.com";
 
+//------question2----//
+ 
+var content = document.getElementById("form-content")
+console.log(content.nodeType);
+var lastName =document.getElementById("last-name")
+console.log(lastName.nodeType);
+var lastName =document.getElementById("last-name")
+console.log(lastName.childNodes);
+var main_content= document.getElementById("main-content")
+console.log(main_content.firstChild);
+var main_content= document.getElementById("main-content")
+console.log(main_content.lastChild);
+var next = document.getElementById("last-name")
+console.log(next.nextElementSibling);
+var pre = document.getElementById("last-name")
+console.log(pre.previousElementSibling);
+var mail = document.getElementById("email")
+console.log(mail.parentNode.nodeType);
+
+
+
